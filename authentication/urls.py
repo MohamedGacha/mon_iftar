c0fa79ac9b85aa1662ee78ab2e56a075905d3ec1
MohamedGacha <34283883+MohamedGacha@users.noreply.github.com>
2025-02-27 23:54:34 +0100

    fix imported view

--- authentication/urls.py
+++ authentication/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('register/', views.register_user, name='register_user'),
+    path('register/', views.RegisterUserAPIView.as_view(), name='register_user'),
     path('first-login/', views.FirstLoginAPIView.as_view(), name='first_login'),
     path('regular-login/', views.RegularLoginAPIView.as_view(), name='regular_login'),
 ]
