544e6560cd214bbe1ad42ae62b1133d30cb0c073
MohamedGacha <34283883+MohamedGacha@users.noreply.github.com>
2025-03-01 16:28:36 +0100

    update whatsapp.py

--- utils/whatsapp.py
+++ utils/whatsapp.py
@@ -1,9 +1,9 @@
+import base64
 from io import BytesIO
 
 import qrcode
 from django.conf import settings
 from twilio.rest import Client
-
 
 def send_whatsapp_message(to_number: str, message: str, console: bool = False):
     if not console:
@@ -26,14 +26,11 @@ def send_whatsapp_message(to_number: str, message: str, console: bool = False):
         # Just print to console
         print(f'Console mode - Message content: {message}')
 
-
 def send_whatsapp_qr_code(to_number, code_unique, date_validite):
     """
     Generate a QR code for the unique code and send a WhatsApp message 
     with the QR code image and a message to the provided number.
     """
-    print(code_unique)
-    pass
     # Generate the QR code image
     qr = qrcode.QRCode(
         version=1,
@@ -50,7 +47,10 @@ def send_whatsapp_qr_code(to_number, code_unique, date_validite):
     img.save(img_byte_arr, format='PNG')
     img_byte_arr.seek(0)
 
-    # Twilio client setup (ensure you have set up your Twilio SID, Auth Token, and WhatsApp number in settings.py)
+    # Encode the image to base64
+    base64_image = base64.b64encode(img_byte_arr.getvalue()).decode('utf-8')
+
+    # Twilio client setup
     client = Client(settings.TWILIO_ACCOUNT_SID, settings.TWILIO_AUTH_TOKEN)
 
     # Message body
@@ -61,9 +61,8 @@ def send_whatsapp_qr_code(to_number, code_unique, date_validite):
         body=message_body,
         from_=f"whatsapp:{settings.TWILIO_WHATSAPP_FROM}",
         to=f"whatsapp:{to_number}",
-        media_url=["data:image/png;base64," +
-                   img_byte_arr.getvalue().encode("base64")],
+        media_url=[f"data:image/png;base64,{base64_image}"],
     )
 
-    # You can return the message SID or handle it as needed
+    # Return the message SID
     return message.sid
